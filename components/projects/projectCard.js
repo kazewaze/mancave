41c616952124c63fb28f76c3533015e42189e12d
Kaycee Ingram <KayceeWilliamIngram@gmail.com>
2022-10-10 20:43:40 -0700

    possibel fix on image render

--- components/projects/projectCard.js
+++ components/projects/projectCard.js
@@ -3,12 +3,12 @@ import Image from 'next/image'
 
 import styles from '../styles/projects.module.css'
 
-export default function ProjectCard({title, imgLocation, imgWidth="100px", imgHeight="100px", imgAlt="Project Logo", projectLocation, sourceLocation, noTitle=false}) {
+export default function ProjectCard({title='', imgLocation, imgWidth="100px", imgHeight="100px", imgAlt="Project Logo", projectLocation, sourceLocation}) {
   return (
     <div className={styles.Card}>
       <Image src={imgLocation} width={imgWidth} height={imgHeight} alt={imgAlt} />
 
-      <h1 className={styles.projectTitle}>{ !noTitle ? title : '' }</h1>
+      <h1 className={styles.projectTitle}>{ title === '' ? '' : title }</h1>
 
       <Link href={projectLocation}>
         Demo
