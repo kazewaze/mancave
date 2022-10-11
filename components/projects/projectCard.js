a41cdeb0fda6ac0b0309310a2f6c1e9d87cd3498
Kaycee Ingram <KayceeWilliamIngram@gmail.com>
2022-10-10 21:09:08 -0700

    more styles again

--- components/projects/projectCard.js
+++ components/projects/projectCard.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import styles from '../styles/projects.module.css'
+import { startOfWeekYear } from 'date-fns'
 
 export default function ProjectCard({title='', imgLocation='', imgWidth="100px", imgHeight="100px", imgAlt="Project Logo", projectLocation, sourceLocation}) {
   return (
@@ -9,16 +10,16 @@ export default function ProjectCard({title='', imgLocation='', imgWidth="100px",
       { imgLocation === '' ? '' : <Image src={imgLocation} width={imgWidth} height={imgHeight} alt={imgAlt} /> }
 
       { title === 'PickMe!' ? (
-          <h1 className={styles.projectTitle}><span style={{color: "#00c14e"}}>{ title }</span></h1>
+          <h1 className={styles.projectTitle}><span style={{color: "#00c14e", fontWeight: '700'}}>{ title }</span></h1>
         ) : (
           <h1 className={styles.projectTitle}>{ title }</h1>
         )
       }
 
-      <Link href={projectLocation}>
+      <Link className={styles.demoLink} href={projectLocation}>
         Demo
       </Link>
-      <Link href={sourceLocation}>
+      <Link className={styles.srcLink} href={sourceLocation}>
         Source Code
       </Link>
     </div>
