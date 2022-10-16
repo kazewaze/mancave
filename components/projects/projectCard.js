499bba07946d00ddb5759adb207340f9e698a86e
Kaycee Ingram <KayceeWilliamIngram@gmail.com>
2022-10-16 15:43:21 -0700

    ill com back to this later

--- components/projects/projectCard.js
+++ components/projects/projectCard.js
@@ -2,11 +2,10 @@ import Link from 'next/link'
 
 import styles from '../styles/projects.module.css'
 
-export default function ProjectCard({title='', description, projectLocation='', sourceLocation=''}) {
+export default function ProjectCard({title='', projectLocation='', sourceLocation=''}) {
   return (
     <div className={styles.Card}>
       <h1 className={styles.projectTitle}>{ title }</h1>
-      <p className={styles.projectDescription}>{ description }</p>
       <div className={styles.cardLinks}>
         <Link className={styles.demoLink} href={projectLocation}>
           Demo
