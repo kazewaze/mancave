b188802157944913a859a29b1743cc7451284936
Kaycee Ingram <KayceeWilliamIngram@gmail.com>
2022-10-16 12:43:22 -0700

    few style changes

--- components/projects/projectCard.js
+++ components/projects/projectCard.js
@@ -1,27 +1,20 @@
 import Link from 'next/link'
-import Image from 'next/image'
 
 import styles from '../styles/projects.module.css'
-import { startOfWeekYear } from 'date-fns'
+import classes from '../../lib/classes'
 
-export default function ProjectCard({title='', imgLocation='', imgWidth="100px", imgHeight="100px", imgAlt="Project Logo", projectLocation, sourceLocation}) {
+export default function ProjectCard({title='', titleColor='#000000', projectLocation='', sourceLocation=''}) {
   return (
     <div className={styles.Card}>
-      { imgLocation === '' ? '' : <Image src={imgLocation} width={imgWidth} height={imgHeight} alt={imgAlt} /> }
-
-      { title === 'PickMe!' ? (
-          <h1 className={styles.projectTitle}><span style={{color: "#00c14e", fontWeight: '700'}}>{ title }</span></h1>
-        ) : (
-          <h1 className={styles.projectTitle}>{ title }</h1>
-        )
-      }
-
-      <Link className={styles.demoLink} href={projectLocation}>
-        Demo
-      </Link>
-      <Link className={styles.srcLink} href={sourceLocation}>
-        Source Code
-      </Link>
+      <h1 className={classes(styles, projectTitle, {color: titleColor})}>{ title }</h1>
+      <div>
+        <Link className={styles.demoLink} href={projectLocation}>
+          Demo
+        </Link>
+        <Link className={styles.srcLink} href={sourceLocation}>
+          Source
+        </Link>
+      </div>
     </div>
   )
 }
